0c943350722cf1646d730f883b9aeef9310dadb8
liujianqiang1016 <34303272+liujianqiang1016@users.noreply.github.com>
2023-12-09 21:56:50 +0800

    完善呼唤电梯，增加LCD的背光开关

--- main.ts
+++ main.ts
@@ -52,22 +52,25 @@ function DisplayDS3231 () {
     } else {
         I2C_LCD1602.ShowNumber(DS3231.second(), 12, 0)
     }
-    if (key_count > 5) {
+    if (floor_change_state == 2) {
         I2C_LCD1602.ShowString("floor", 0, 1)
         I2C_LCD1602.ShowNumber(current_floor, 6, 1)
         I2C_LCD1602.ShowString("->", 8, 1)
         I2C_LCD1602.ShowNumber(targe_floor, 11, 1)
-    } else {
+    } else if (floor_change_state == 1) {
         I2C_LCD1602.ShowString("floor ", 0, 1)
         I2C_LCD1602.ShowNumber(current_floor, 6, 1)
         I2C_LCD1602.ShowString("     ", 7, 1)
+        floor_change_state = 0
     }
 }
 let motor: stepperMotor.Motor = null
 let targe_floor = 0
 let current_floor = 0
+let floor_change_state = 0
+floor_change_state = 1
+let lcd_light_flag = 1
 let key_count = 0
-key_count = 0
 let dht11_count = 0
 current_floor = 1
 targe_floor = 1
@@ -90,8 +93,10 @@ basic.forever(function () {
         if (key_count > 5) {
             pins.digitalWritePin(DigitalPin.P13, 1)
             targe_floor = 1
+            floor_change_state = 2
             ElevatorUpAndDown(targe_floor - current_floor)
             current_floor = 1
+            floor_change_state = 1
             pins.digitalWritePin(DigitalPin.P13, 0)
             key_count = 0
         }
@@ -100,8 +105,10 @@ basic.forever(function () {
         if (key_count > 5) {
             pins.digitalWritePin(DigitalPin.P14, 1)
             targe_floor = 2
+            floor_change_state = 2
             ElevatorUpAndDown(targe_floor - current_floor)
             current_floor = 2
+            floor_change_state = 1
             pins.digitalWritePin(DigitalPin.P14, 0)
             key_count = 0
         }
@@ -110,14 +117,25 @@ basic.forever(function () {
         if (key_count > 5) {
             pins.digitalWritePin(DigitalPin.P15, 1)
             targe_floor = 3
+            floor_change_state = 2
             ElevatorUpAndDown(targe_floor - current_floor)
             current_floor = 3
+            floor_change_state = 1
             pins.digitalWritePin(DigitalPin.P15, 0)
             key_count = 0
         }
     } else if (pins.digitalReadPin(DigitalPin.P8) == 1) {
         pins.digitalWritePin(DigitalPin.P16, 1)
         basic.pause(1000)
+        if (lcd_light_flag == 0) {
+            lcd_light_flag = 1
+            I2C_LCD1602.BacklightOn()
+            I2C_LCD1602.clear()
+        } else {
+            lcd_light_flag = 0
+            I2C_LCD1602.BacklightOff()
+            I2C_LCD1602.clear()
+        }
         pins.digitalWritePin(DigitalPin.P16, 0)
     } else {
         key_count = 0
